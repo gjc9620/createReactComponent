c3cbe3b0a331944310326958dcab99c1a776438f
gujiacheng07691 <gujiacheng07691@hellobike.com>
2020-03-24 12:21:07 +0800

    js

--- reactTemplate.js
+++ reactTemplate.js
@@ -5,11 +5,6 @@ module.exports = function reactTemplate ({
   name = 'template',
 }) {
   return `import React, { Component, PropTypes } from 'react';
-import Iconfont from 'components/Iconfont';
-import fetchAPI from 'utils/fetch';
-import JSBridge from 'za-jsbridge';
-import classnames from 'classnames';
-import { connect } from 'react-redux';
 
 class ${name} extends React.Component {
   // constructor(props) {
